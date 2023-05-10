f97d05895e3b72ffb6f03aab8ed38f7b382c99c3
silvijatub <125203370+silvijatub@users.noreply.github.com>
2023-05-10 09:45:20 +0300

    Update product_page.php

--- product_page.php
+++ product_page.php
@@ -123,36 +123,22 @@ require_once 'connection.php';
                }
         ?>
 
+ <?php include 'repeatingElements/footer.php'; ?>
 
-  
-
-       
-
-
-
-
-        <div class="kontaktai">
-        <div class="light16Text">
-            <p class="lightBold20Text">Susisiekite su mumis:</p>
-            <p>kodokepykla@gmail.com</p>
-            <p>+37065658564</p>
-            <div>
-                <a href="https://www.facebook.com"><img class="socials" src="https://lh3.googleusercontent.com/LevHz4AOP4W_CeqKZvs8bX4hr79W_iYtyn_0D9doN7LmLvbhdUU0eP0iGnbLBT9Pe_I=w2400"></a>
-                <a href="https://www.instagram.com"><img class="socials" src="https://lh4.googleusercontent.com/RIUGBPCHD63KkA8iWPLKbzXgXTH5Y86FBQggGL_ZyJpSWz5IvSahyFw8Wjttsepyr8Q=w2400"></a>
+    <div id="myModal" class="modal">
+            <div class="modal-content">
+                <button class="close" onclick="closeDialog()">&times;</button>
+                <p class="dark16Text">Šią prekę gali įvertinti tik ją įsigiję klientai.</p>
+                <p class="dark16Text">Kviečiame užsisakyti šią prekę dabar!</p>
             </div>
-            <p>@ Kodo Kepykla 2023</p>
         </div>
-        <div class="light16Text">
-            <p class="lightBold20Text">Mus rasite:</p>
-            <p>Studentų g. 50,</p>
-            <p>Kaunas</p>
-        </div>
-    </div>
-       
+
         <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.js"></script>
         <script type="text/javascript" src="functions.js"></script>
         <script type="text/javascript" src="items.js"></script>
+        <script type="text/javascript" src="modalDialog.js"></script>
 
+   
    
    </body>
 </html>
